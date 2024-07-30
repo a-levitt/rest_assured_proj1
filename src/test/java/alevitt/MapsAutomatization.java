0803a95689cba748616e89df036a00045b49a9d9
a-levitt <artemis.levitt@gmail.com>
2024-07-30 13:08:36 +0200

    body request put in separated package and class

--- src/test/java/alevitt/MapsAutomatization.java
+++ src/test/java/alevitt/MapsAutomatization.java
@@ -1,5 +1,6 @@
 package alevitt;
 
+import files.payload;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -13,23 +14,7 @@ public class MapsAutomatization {
                 .log().all()
                 .queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
-                .body("{\r\n" +
-                "            \"location\": {\r\n" +
-                "               \"lat\": -38.383494,\n" +
-                "               \"lng\": -33.427362\n" +
-                "             },\r\n" +
-                "             \"accuracy\": 50,\r\n" +
-                "             \"name\": \"Rahul Shetty Academy\",\r\n" +
-                "             \"phone_number\": \"(+91) 983 893 3937\",\r\n" +
-                "             \"address\": \"29, side layout, cohen 09\",\r\n" +
-                "             \"types\": [\r\n" +
-                "               \"shoe park\",\r\n" +
-                "               \"shop\"\r\n" +
-                "             ],\r\n" +
-                "             \"website\": \"http://rahulshettyacademy.com\",\r\n" +
-                "             \"language\": \"French-IN\"\r\n" +
-                "          }\r\n" +
-                "     ")
+                .body(payload.addPlace())
         .when()
                 .post("maps/api/place/add/json")
         .then()
